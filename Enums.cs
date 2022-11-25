a1b366cd7a135e514b26b03c3224477dd8e706dd
Sebastian Kukawski <sk83505@stud.uph.edu.pl>
2022-11-25 20:58:06 +0100

    first commit

---
+++ Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace NosGame;
+
+public enum Games
+{
+    Fishpond = 0,
+    Sawmill = 1
+}
+
+public enum States
+{
+    Idle,
+    StartingGame,
+    StartGame,
+    Playing,
+    QtEvent,
+    Failed,
+    GettingReward,
+    SelectingLevel,
+    Finished,
+    ClosingRewardScreen,
+    CouponRefill,
+}
+
+public enum CouponRefillStates
+{
+    CloseGame,
+    UseCoupon,
+    AcceptCoupon,
+    Finished
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Input;
+using NosGame.MVVM;
+using NosGame.Utils;
+using Window = System.Windows.Window;
+
+namespace NosGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadPoints.LoadArray();
+            DataContext = new ViewModel();
+        }
+
+        private void PreviewText(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        
+        private static readonly Regex Regex = new("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !Regex.IsMatch(text);
+        }
+    }
+}
